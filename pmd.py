3c0cf66741bf3a0c7b4bcf976ad896b0c6a0e273
nourmethlouthi <99082504+nourmethlouthi@users.noreply.github.com>
2023-08-21 09:06:28 +0100

    Update pmd.py

--- pmd.py
+++ pmd.py
@@ -1,7 +1,7 @@
 import argparse
 import subprocess
 import json
-import pathlib
+import os.path
 from jinja2 import Environment, FileSystemLoader
 import os
 import subprocess
@@ -12,86 +12,55 @@ from jinja2 import Template
 import http.server
 import socketserver
 import webbrowser
-
 import sys
-def run_semgrep(rules,code):
-        semgrep_command = [
-                "semgrep",
-                "--config",
-                rules,
-                "--junit-xml",
-                code,"--quiet"
-        ]
-        junit_output = subprocess.check_output(semgrep_command )
-        
-        return (junit_output)
 
+ERROR = "ERROR" 
+WARNING = "WARNING"
+INFO = "INFO" 
 
-def pmd(fileDirectory,ruleset):
-# Construct the command to run PMD
-# Split the command and arguments into separate list elements
-    command = ['pmd', 'check',"-f","json", fileDirectory, '-R', ruleset,"--no-fail-on-violation"]
-
+def run_tool (command):
     try:
         result = subprocess.run(command,
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE,
                                 text=True,
-                                check=True)  # Raise CalledProcessError if command fails
-
-    # Print the captured standard output and standard error
-     
-        json_content = result.stdout
-
-        # Parse the captured JSON content
-        parsed_json = json.loads(json_content)
-        
-        # Write the parsed JSON to a file (stdout)
-        return(parsed_json)
-
-
+                                check=True)
+        return result.stdout
     except subprocess.CalledProcessError as e:
-    # Print the error message and captured error output if the command fails
         print("Error:", e)
-        print("PMD Error Output:")
+        print("Error Output:")
         print(e.stderr)
-    
-def is_windows_path(path):
-    
-    return '\\' in path
 
-def is_posix_path(path):
-    return '/' in path
-def normalizer(x):
+def run_pmd(fileDirectory,ruleset):
+    pmd_command = ['pmd', 'check',"-f","json", fileDirectory, '-R', ruleset,"--no-fail-on-violation"]
+    #!!!!!!!!!!!!problem if runtool prints error and returns nothing
+    parsed_json = json.loads(run_tool(pmd_command))
+    return(parsed_json)
+
+def run_semgrep(rules,code):
+    semgrep_command = ["semgrep","--config",rules,"--junit-xml",code,"--quiet"]
+    return (run_tool(semgrep_command))
+
+def severity_normalizer(x):
     match x:
         case 1:
-            severity = "ERROR" 
+            severity = ERROR
         case 2 | 3:
-            severity = "WARNING"
+            severity = WARNING
         case 4 | 5:
-            severity = "INFO" 
-
-    return severity
-def path_normalizer(path):
-    
-    if is_windows_path(path):
-        return pathlib.PureWindowsPath(path).name
-    else :
-        return pathlib.PurePosixPath(path).name
+            severity = INFO
+    # which default value to use ?????????????
+        case _:
+            severity = "not defined severity"
+    return severity 
 
 def pmd_parser(non_parsed_report ):
-    
-           
-        
-    
-    result_list = []
-    result_dict = {"violations": result_list}
-    
+    violations = []
     for dict in non_parsed_report["files"]:
         for violation in dict['violations']:
-            severity = normalizer(violation['priority'])
+            severity = severity_normalizer(violation['priority'])
             dictionary = {
-                'file': path_normalizer( dict['filename']),
+                'file': os.path.basename( dict['filename']),
                 'name': violation['rule'],
                 'priority': violation['priority'],
                 'message': violation['description'],
@@ -99,57 +68,67 @@ def pmd_parser(non_parsed_report ):
                 
                 'severity': severity
             }
-            result_dict['violations'].append(dictionary)
-            
-    return result_dict
+            violations.append(dictionary)    
+    return {"violations":violations}
+
+def search_tag(parent,child):
+    try:
+        return parent.find(child)
+    except AttributeError:
+        print("check the parent name")
+    except TypeError:
+        print("check the child name")
+
+def search_tags(parent,child):
+    try:
+        return parent.findall(child)
+    except AttributeError:
+        print("check the parent name")
+    except TypeError:
+        print("check the child name")
+
+
 def semgrep_parser(junit_output):
         root = ET.fromstring(junit_output)
-        models=root.findall(".//testcase")
-        list=[]
-        dic = {"violations":list}
+        models=search_tags(root,".//testcase")
+        violations=[]
         for i in models:
-                list.append({"name":i.get('name'),
+                f=search_tag(i,"failure")
+                violations.append({"name":i.get('name'),
                         "file":i.get('file'),
                         "line":i.get('line'),
-                        "severity":i.find('failure').get('type'),
-                        "message":i.find('failure').get('message')
+                        "severity":f.get('type'),
+                        "message":f.get('message')
                 })
-        return (dic)
-def score_calculator(report_list):
+        return {"violations":violations}
+
+def result_dic(violations):
     warning_count = 0
     info_count = 0
     error_count = 0
-
-    # Read JSON input from stdin and process each JSON object
-
-    for report in report_list:
-        violations = report.get("violations", [])
-            # Iterate through violations and count severity levels
-        for violation in violations:
-                
-            severity = violation.get("severity", "")
-            if severity == "WARNING":
-                warning_count += 1
-            elif severity == "INFO":
-                    info_count += 1
-            elif severity == "ERROR":
-                    error_count += 1
-    dic_score = {"WARNING": warning_count, "INFO": info_count, "ERROR": error_count}
-## calculate the score 
-    if error_count > 0:
-        
-        dic_score ["score"] =""
-
-        dic_score ["result"] = False 
-        return dic_score
-        
-    else :
-        score= 10 - warning_count - info_count*0.5
-        dic_score ["score"] = score
-        result = score>=5
-        dic_score ["result"] = result 
    
-        return dic_score
+    for violation in violations:       
+        severity = violation.get("severity", "")
+        if severity == WARNING:
+            warning_count += 1
+        elif severity == INFO:
+            info_count += 1
+        elif severity == ERROR:
+            error_count += 1
+    return {"WARNING": warning_count, "INFO": info_count, "ERROR": error_count}
+
+def score_calculator(dic):
+    dic_score={}
+    dic_score ["score"] = 10
+    if dic["ERROR"] > 0:
+        dic_score ["score"] = 0
+        dic_score ["result"] = False 
+    else :
+        #score could be negative !!!!!!!!!!!
+        s=10 - dic["WARNING"]- dic["INFO"]*0.5
+        dic_score ["score"] = s > 0 ? s : 0
+        dic_score ["result"] = dic_score ["score"]>=5
+    return dic_score
             
 def html_generator(parsed_report_pmd,parsed_report_semgrep,score_dict,path):
     env = Environment(loader=FileSystemLoader('.'))
@@ -157,8 +136,6 @@ def html_generator(parsed_report_pmd,parsed_report_semgrep,score_dict,path):
     warning_count = score_dict["WARNING"] 
     info_count = score_dict["INFO"]
     error_count = score_dict["ERROR"]
-
-    
     rendered_html = template.render(
         warning_count=warning_count,
         info_count=info_count,
@@ -167,15 +144,13 @@ def html_generator(parsed_report_pmd,parsed_report_semgrep,score_dict,path):
         parsed_report_semgrep=parsed_report_semgrep,  
         score = score_dict["score"],
         passed = score_dict["result"])
-    
-
     # Write the rendered HTML to a file
     path.write(rendered_html)
 
    
     
 def main():
-    # Create an ArgumentParser object to handle command-line arguments
+# Create an ArgumentParser object to handle command-line arguments
     parser = argparse.ArgumentParser(description='Script that starts PMD command')
 
 # Define the expected command-line arguments
@@ -193,7 +168,7 @@ def main():
 
     ruleset_pmd = args.rulesetpmd
     ruleset_semgrep = args.rulesetsemgrep
-    unparsed_report_pmd = pmd(fileDirectory,ruleset_pmd)
+    unparsed_report_pmd = run_pmd(fileDirectory,ruleset_pmd)
     unparsed_report_semgrep = run_semgrep(ruleset_semgrep,fileDirectory)
     parsed_report_pmd = pmd_parser(unparsed_report_pmd)
     parsed_report_semgrep = semgrep_parser(unparsed_report_semgrep)
@@ -202,13 +177,13 @@ def main():
     report_list = []
     report_list.append(parsed_report_pmd)
     report_list.append(parsed_report_semgrep) 
-    score_dict= score_calculator(report_list)
-    print(score_dict["result"])
+    result= result_dic(report_list)
+    score=score_calculator(result)
+
+    print(score)
     html_generator(parsed_report_pmd,parsed_report_semgrep,score_dict,path)
     
 
 
 if __name__ == "__main__":
     main()
-
-
